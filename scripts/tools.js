acfbb0529c9713cb01a318f5263db5cbebf8bebf
ymiuk <ymiuk@bu.edu>
2023-04-29 19:10:47 -0400

    tools updated

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,31 +1,18 @@
-const stepArr = Array.from(document.querySelectorAll('.step'))
-/*
-stepArr[0].addEventListener('click', () => {
-  window.location.href = '../2-coffeeStyles.html'
-})
+const stepArr = Array.from(document.querySelectorAll(".step"));
+const toolChoice = Array.from(document.getElementsByName("tool-type"));
 
-stepArr[1].addEventListener('click', () => {
-  window.location.href = '../4-addingFlavors.html'
-})
-*/
+toolChoice.forEach((choice) => {
+  choice.addEventListener("change", getToolInfo);
+});
 
-const toolChoice = Array.from(document.getElementsByName('tool-type'));
-
-const saveBtn = document.getElementById('save-choices');
-
-toolChoice[0].addEventListener('change', getToolInfo);
-toolChoice[1].addEventListener('change', getToolInfo);
-toolChoice[2].addEventListener('change', getToolInfo);
-toolChoice[3].addEventListener('change', getToolInfo);
-toolChoice[4].addEventListener('change', getToolInfo);
-
-saveBtn.addEventListener('click', getToolInfo );
-  
-  function getToolInfo(){
-    toolChoice.forEach( tool => {
-      if (tool.checked) {
-        localStorage.setItem('tool choice', tool.value )
-        console.log('the tool you selected for your coffee is ' + tool.value);
-      }
-    });
+function getToolInfo() {
+  toolChoice.forEach((tool) => {
+    if (tool.checked) {
+      localStorage.setItem("toolChoice", tool.value);
+      console.log(`Tool selected: ${tool.value}`);
+    }
+  });
 }
+
+const saveBtn = document.getElementById("save-choices");
+saveBtn.addEventListener("click", getToolInfo);
